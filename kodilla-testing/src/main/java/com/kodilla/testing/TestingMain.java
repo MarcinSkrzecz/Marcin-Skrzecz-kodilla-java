edf158eb26426bac27cbe78b0ef481387b9716d0
MarcinSkrzecz <marpiowawa@o2.pl>
2020-04-09 21:48:35 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+import java.sql.SQLOutput;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("1.0 test OK");
+        } else {
+            System.out.println("1.0 Error!");
+        }
+
+        //--------------------------//
+
+        Calculator calculator = new Calculator(15, 5);
+
+        int calculatorAdd = calculator.getAdd();
+        int calculatorSubstract = calculator.getSubstract();
+
+        if (calculatorAdd == 20) {
+            System.out.println("2.1 test OK");
+        } else {
+            System.out.println("2.1 Error!");
+        }
+
+        if (calculatorSubstract == 10) {
+            System.out.println("2.2 test OK");
+        } else {
+            System.out.println("2.2 Error!");
+        }
     }
 }
