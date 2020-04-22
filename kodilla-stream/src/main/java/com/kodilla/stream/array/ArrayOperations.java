4063e6ba5e656b9dd90dd1adc272c3db6be7a51f
MarcinSkrzecz <marpiowawa@o2.pl>
2020-04-22 14:39:18 +0200

    7.5 correction 22.04.2020

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -6,16 +6,23 @@ import java.util.stream.IntStream;
 public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
-        IntStream.range(0, numbers.length)
-                .filter(n -> n != 0)
-                .map(id -> numbers[id])
-                .forEach(System.out::println);
+        if(numbers.length > 0) {
+            IntStream.range(0, numbers.length)
+                    .filter(n -> n != 0)
+                    .map(id -> numbers[id])
+                    .forEach(System.out::println);
+        } else {
+            System.out.println("Table is empty");
+        }
 
-        OptionalDouble averageList = IntStream.range(0, numbers.length)
-                .mapToDouble(id -> numbers[id])
-                .average().orElse(0.0);
-        double averageDouble = averageList.getAsDouble();
-        return averageDouble;
+        if(numbers.length > 0) {
+            OptionalDouble averageList = IntStream.range(0, numbers.length)
+                    .mapToDouble(id -> numbers[id])
+                    .average();
+            double averageDouble = averageList.getAsDouble();
+            return averageDouble;
+        } else {
+            return 0;
+        }
     }
-
 }
