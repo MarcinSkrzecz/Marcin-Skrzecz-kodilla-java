c341c3fee155960ac4c19aba52009ee6b7f06466
MarcinSkrzecz <marpiowawa@o2.pl>
2020-06-10 14:05:10 +0200

    third task fix

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,26 +1,34 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
+    @Autowired
+    Board board;
+
     @Bean
     public Board getBoard() {
         return new Board(toDoTaskList(), inProgressTaskList(), doneTaskList());
     }
 
-    @Bean
+    @Bean(name = "toDoTaskList")
+    @Scope("prototype")
     public TaskList toDoTaskList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "inProgressTaskList")
+    @Scope("prototype")
     public TaskList inProgressTaskList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "doneTaskList")
+    @Scope("prototype")
     public TaskList doneTaskList() {
         return new TaskList();
     }
