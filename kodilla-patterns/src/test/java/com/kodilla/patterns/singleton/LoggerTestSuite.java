e02791ad3b8ef76ec8670a33718aa3595a34f97d
MarcinSkrzecz <marpiowawa@o2.pl>
2020-06-12 20:09:05 +0200

    15.1 first task + last module correction

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+
+    @BeforeClass
+    public static void createLog() {
+        Logger.getInstance().log("First Log");
+    }
+
+    @Test
+    public void testGetLastLog() {
+        //Given
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("First Log",lastLog);
+    }
+}
--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,18 +1,29 @@
 package com.kodilla.spring.portfolio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
+
+    @Qualifier("toDoTaskList")
     @Autowired
-    Board board;
+    private TaskList toDoList;
+
+    @Qualifier("inProgressTaskList")
+    @Autowired
+    private TaskList inProgressTaskList;
+
+    @Qualifier("doneTaskList")
+    @Autowired
+    private TaskList doneTaskList;
 
     @Bean
     public Board getBoard() {
-        return new Board(toDoTaskList(), inProgressTaskList(), doneTaskList());
+        return new Board(toDoList, inProgressTaskList, doneTaskList);
     }
 
     @Bean(name = "toDoTaskList")
